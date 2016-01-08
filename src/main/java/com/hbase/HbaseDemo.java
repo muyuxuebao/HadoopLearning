2ac3768f30d22f35234a754e9860b5e32a4a308e
muyux <muyuxuebao@163.com>
2016-01-08 16:48:02 +0800

    first commit

--- src/main/java/com/hbase/HbaseDemo.java
+++ src/main/java/com/hbase/HbaseDemo.java
@@ -7,8 +7,6 @@ import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.HTableInterface;
-import org.apache.hadoop.hbase.client.HTablePool;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
