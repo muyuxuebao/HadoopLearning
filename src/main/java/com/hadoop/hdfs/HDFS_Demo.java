dd3471b7ff8941cdaf608b029ec85c202b1126c6
muyux <muyuxuebao@163.com>
2016-01-25 20:30:34 +0800

    first commit

--- src/main/java/com/hadoop/hdfs/HDFS_Demo.java
+++ src/main/java/com/hadoop/hdfs/HDFS_Demo.java
@@ -5,9 +5,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.IOUtils;
 
 import org.junit.Before;
@@ -22,6 +20,36 @@ public class HDFS_Demo {
     @Before
     public void init() throws URISyntaxException, IOException, InterruptedException {
         fs = FileSystem.get(new URI("hdfs://itcast01:9000"), new Configuration(), "root");
+    }
+
+    /**
+     * 获取块的信息
+     */
+    @Test
+    public void testGetBlockInfo() throws IOException {
+        Configuration configuration = new Configuration();
+        Path path = new Path("/Bugs.2014.mp4");
+        FileStatus fileStatus = fs.getFileStatus(path);
+
+        BlockLocation[] blockLocations = fs.getFileBlockLocations(fileStatus, 0, fileStatus.getLen());
+
+        System.out.println(blockLocations.length);
+        for (BlockLocation blockLocation : blockLocations) {
+            System.out.println(String.format("Offset:%10d, Length:%10d", blockLocation.getOffset(), blockLocation.getLength()));
+            String[] hosts = blockLocation.getHosts();
+            String[] names = blockLocation.getNames();
+            System.out.println("hosts:");
+            for (String host : hosts) {
+                System.out.println(host);
+            }
+
+            System.out.println("names:");
+            for (String name : names) {
+                System.out.println(name);
+            }
+            System.out.println("===================================");
+
+        }
     }
 
     @Test
--- src/main/java/com/hadoop/hdfs/HDFS_HA_Demo.java
+++ src/main/java/com/hadoop/hdfs/HDFS_HA_Demo.java
@@ -1,9 +1,7 @@
 package com.hadoop.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -29,7 +27,35 @@ public class HDFS_HA_Demo {
 
         fs = FileSystem.get(new URI("hdfs://ns1"), conf, "root");
     }
+    /**
+     * 获取块的信息
+     */
+    @Test
+    public void testGetBlockInfo() throws IOException {
+        Configuration configuration = new Configuration();
+        Path path = new Path("/Bugs.2014.mp4");
+        FileStatus fileStatus = fs.getFileStatus(path);
 
+        BlockLocation[] blockLocations = fs.getFileBlockLocations(fileStatus, 0, fileStatus.getLen());
+
+        System.out.println(blockLocations.length);
+        for (BlockLocation blockLocation : blockLocations) {
+            System.out.println(String.format("Offset:%10d, Length:%10d", blockLocation.getOffset(), blockLocation.getLength()));
+            String[] hosts = blockLocation.getHosts();
+            String[] names = blockLocation.getNames();
+            System.out.println("hosts:");
+            for (String host : hosts) {
+                System.out.println(host);
+            }
+
+            System.out.println("names:");
+            for (String name : names) {
+                System.out.println(name);
+            }
+            System.out.println("===================================");
+
+        }
+    }
     @Test
     public void testUpload() throws IOException {
         FSDataOutputStream out = fs.create(new Path("/jdk_upload.tar.gz"));
