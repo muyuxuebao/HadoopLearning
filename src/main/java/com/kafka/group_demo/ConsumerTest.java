3f37147bb9190f6a10c8023310ba6ff1cdcdc251
yinliang7 <yinliang@jd.com>
2018-07-29 14:05:00 +0800

    step

---
+++ src/main/java/com/kafka/group_demo/ConsumerTest.java
@@ -0,0 +1,7 @@
+package com.kafka.group_demo;
+
+/**
+ * Created by muyux on 2018/7/29.
+ */
+public class ConsumerTest {
+}
---
+++ src/main/java/com/kafka/group_demo/GroupConsumerTest.java
@@ -0,0 +1,8 @@
+package com.kafka.group_demo;
+
+/**
+ * Created by muyux on 2018/7/29.
+ */
+public class GroupConsumerTest {
+
+}
